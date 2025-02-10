9ed8c31e5790e3eda9761c96e04b0ee91de82240
Doug Ross <122139227+RhinoBytes@users.noreply.github.com>
2025-02-10 15:36:23 -0500

    exercise 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let result = "";
 
+  if (num < 0) {
+    return "ERROR";
+  }
+  // console.log(string, num);
+  for (let i = 1; i <= num; i++) {
+    result += string;
+    // console.log(result);
+  }
+  // console.log(result);
+  return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+  let result = 0;
+  for (let i = num1; i <= num2; i++) {
+    result += i;
+  }
+  return result;
 
 };
 
